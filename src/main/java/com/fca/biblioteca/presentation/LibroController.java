57827dc36c07324b6cc8f527ce9194c7a88e08ea
Aldir0day <erick.alcala.velasco@gmail.com>
2025-09-02 13:28:49 -0600

    Anotaciones de Java en las clases

---
+++ src/main/java/com/fca/biblioteca/presentation/LibroController.java
@@ -0,0 +1,11 @@
+package com.fca.biblioteca.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(path = "/Libros", method = RequestMethod.GET)
+public class LibroController {
+
+}
